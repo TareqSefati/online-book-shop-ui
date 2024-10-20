9c6d37817e1c2824b7ac0a73c51112271819666b
Tareq Sefati <tareq.sefati@gmail.com>
2024-10-21 00:58:52 +0600

    Feat: Implement banner section.

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,26 +6,29 @@ export default function Banner() {
 			<div className="hero bg-base-200 shadow-xl rounded-lg mt-5 px-5">
 				<div className="hero-content flex-col lg:flex-row-reverse">
 					<img
-						src="/images/banner-3.webp"
+						src="https://i.ibb.co.com/2SwZLr2/banner-open-book-logo.jpg"
 						className="lg:max-w-xl md:max-w-lg rounded-lg shadow-2xl"
 					/>
 					<div className="space-y-10">
-                        <div>
-                            <div className="text-2xl font-semibold">
-                                Let&apos;s update yourself with latest technology with...<br />
-                            </div>
-                            <div className="mt-5 text-4xl font-bold text-slate-700">
-								Code Course Academy
-							</div>
-                        </div>
 						<div>
-                            <Link><a className="btn btn-neutral">Enroll</a></Link>
-                        </div>
+							<div className="text-2xl text-slate-500">
+								Books build minds, not debt.<br />
+							</div>
+							<div className="mt-5 text-2xl text-slate-600">
+								Knowledge is worth more than money.<br />
+							</div>
+							<div className="mt-5 text-4xl font-bold text-slate-700">
+								Books are an investment, not a luxury.
+							</div>
+						</div>
+						<div>
+							<Link><a className="btn btn-neutral">Buy Now</a></Link>
+						</div>
 					</div>
 				</div>
 			</div>
 
-            {/* This immediate following code has problem of z indexing with drop dwon menu */}
+			{/* This immediate following code has problem of z indexing with drop dwon menu */}
 			{/* <div className="shadow-xl mt-5 relative before:absolute before:w-full before:h-full before:inset-0 before:bg-black before:opacity-50 before:z-10">
 				<img
 					src="/images/banner-3.webp"
