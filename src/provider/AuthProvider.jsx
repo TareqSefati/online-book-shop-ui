34de3cd514c11bc8d175148891952fb46698c0c1
Tareq Sefati <tareq.sefati@gmail.com>
2024-10-24 18:25:58 +0600

    Feat: Get user data from mongodb & set it auth context provider after firebase loging.

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -15,6 +15,7 @@ const auth = getAuth(app);
 
 export default function AuthProvider({ children }) {
 	const [user, setUser] = useState([]);
+	const [dbUser, setDbUser] = useState([]);
 	const [loading, setLoading] = useState(true);
 
 	const createUser = (email, password) => {
@@ -47,10 +48,29 @@ export default function AuthProvider({ children }) {
 		return signOut(auth);
 	};
 
+	const getUserFromDb = async(firebaseUser)=>{
+		const url = `${import.meta.env.VITE_BACKEND_USER_BY_UID_URL}/${firebaseUser.uid}`;
+		console.log("User from db url: ", url);
+		const user = await fetch(url)
+					.then((res)=>res.json())
+					.then((data)=>{
+						// console.log("User data from DB: ", data);
+						return data;
+					})
+					.catch((err)=>{
+						console.log("Error: ", err);
+						return null;
+					});
+		return user;
+	}
+
 	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+		const unsubscribe = onAuthStateChanged(auth, async(currentUser) => {
 			console.log(currentUser);
 			setUser(currentUser);
+			const userFromDb = await getUserFromDb(currentUser);
+			console.log("User data from DB: ", userFromDb);
+			setDbUser(userFromDb);
 			setLoading(false);
 		});
 		return () => {
@@ -59,6 +79,7 @@ export default function AuthProvider({ children }) {
 	}, []);
 	const authInfo = {
 		user,
+		dbUser,
 		loading,
 		createUser,
 		signIn,
