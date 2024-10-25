8fb3f43a6fe30c29a4e5b527754dd14b07c5229c
Tareq Sefati <tareq.sefati@gmail.com>
2024-10-26 03:05:57 +0600

    Fix: Fising profile image issue for profile view window.

--- src/components/ViewProfile.jsx
+++ src/components/ViewProfile.jsx
@@ -1,15 +1,17 @@
 
 export default function ViewProfile(props) {
     const { user } = props;
+    // card card-side
     return (
         <div>
-            <div className="card card-side bg-base-100 shadow-xl">
-                <figure>
-                    <img className="rounded-full"
-                        src={user.photoUrl}
-                        alt="profile image" />
-                </figure>
-                <div className="card-body">
+            <div className="bg-base-100 shadow-xl mt-10 flex flex-col md:flex-row lg:flex-row items-center justify-center gap-10 rounded-lg">
+                <div>
+                    <img className="rounded-full size-52 mb-2 shadow-xl"
+                    src={user.photoUrl}
+                    alt="profile image" />
+                    <hr />
+                </div>
+                <div className="mb-4">
                     <h2 className="card-title">{user.name}</h2>
                     <p>Email: {user.email}</p>
                     <p>Address: {user.address}</p>
